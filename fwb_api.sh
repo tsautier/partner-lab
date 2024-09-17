1290f7692a6fab43d6efcece6e4281bbe7e34ed6
ibsina <ibsina@gmail.com>
2024-09-17 16:44:52 +0800

    aa

--- fwb_api.sh
+++ fwb_api.sh
@@ -21,31 +21,31 @@ done
 daftarid=("1 2 3 4 5 6 7 8 10 11 12 13 14 15 16 17 18 19 20")
 for idku in ${daftarid[@]};
 do
-curl -k http://api-ml-demo:8090/news?id=$idku -H "Content-Type: application/json"
+curl -k http://fwb-api.fortinet.demo:8090/news?id=$idku -H "Content-Type: application/json"
 done
 
 
 for k in {1..11}
 do
 curldata="{\"user\":\"ahmad $k\"}"
-curl http://api-ml-demo:8090/love/food -k -H "Content-Type: application/json" -d "$curldata"
-curl http://api-ml-demo:8090/love/drink -k -H "Content-Type: application/json" -d "$curldata"
+curl http://fwb-api.fortinet.demo:8090/love/food -k -H "Content-Type: application/json" -d "$curldata"
+curl http://fwb-api.fortinet.demo:8090/love/drink -k -H "Content-Type: application/json" -d "$curldata"
 done
 
 
 
-curl http://api-ml-demo:8090/101/ -k -H "Content-Type: application/json"
-curl http://api-ml-demo:8090/102/ -k -H "Content-Type: application/json"
-curl http://api-ml-demo:8090/1101/ -k -H "Content-Type: application/json"
-curl http://api-ml-demo:8090/1102/ -k -H "Content-Type: application/json"
-curl http://api-ml-demo:8090/1201/ -k -H "Content-Type: application/json"
-curl http://api-ml-demo:8090/1202/ -k -H "Content-Type: application/json"
-curl http://api-ml-demo:8090/1011/ -k -H "Content-Type: application/json"
-curl http://api-ml-demo:8090/1012/ -k -H "Content-Type: application/json"
-curl http://api-ml-demo:8090/11011/ -k -H "Content-Type: application/json"
-curl http://api-ml-demo:8090/11012/ -k -H "Content-Type: application/json"
-curl http://api-ml-demo:8090/12011/ -k -H "Content-Type: application/json"
-curl http://api-ml-demo:8090/12012/ -k -H "Content-Type: application/json"
+curl http://fwb-api.fortinet.demo:8090/101/ -k -H "Content-Type: application/json"
+curl http://fwb-api.fortinet.demo:8090/102/ -k -H "Content-Type: application/json"
+curl http://fwb-api.fortinet.demo:8090/1101/ -k -H "Content-Type: application/json"
+curl http://fwb-api.fortinet.demo:8090/1102/ -k -H "Content-Type: application/json"
+curl http://fwb-api.fortinet.demo:8090/1201/ -k -H "Content-Type: application/json"
+curl http://fwb-api.fortinet.demo:8090/1202/ -k -H "Content-Type: application/json"
+curl http://fwb-api.fortinet.demo:8090/1011/ -k -H "Content-Type: application/json"
+curl http://fwb-api.fortinet.demo:8090/1012/ -k -H "Content-Type: application/json"
+curl http://fwb-api.fortinet.demo:8090/11011/ -k -H "Content-Type: application/json"
+curl http://fwb-api.fortinet.demo:8090/11012/ -k -H "Content-Type: application/json"
+curl http://fwb-api.fortinet.demo:8090/12011/ -k -H "Content-Type: application/json"
+curl http://fwb-api.fortinet.demo:8090/12012/ -k -H "Content-Type: application/json"
 
 
 
