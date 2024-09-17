6060503299222435267ef8cea408d3b493fb5751
ibsina <ibsina@gmail.com>
2024-09-17 16:46:44 +0800

    aa

--- fad_api.sh
+++ fad_api.sh
@@ -13,7 +13,7 @@ do
  then
   curldata="{\"login\":\"acbde$k\",\"password\":\"my_password$k\",\"loginid\":$RANDOM ,\"rights\": [ \"sales\", \"finance\", \"admin\" ]}"
  fi
- curl http://fad-api.fortinet.demo:8090/sales/ -k -H "Content-Type: application/json" --data-raw "$curldata"
+ curl http://fad-api.fortinet.demo/sales/ -k -H "Content-Type: application/json" --data-raw "$curldata"
 done
 
 
@@ -21,31 +21,31 @@ done
 daftarid=("1 2 3 4 5 6 7 8 10 11 12 13 14 15 16 17 18 19 20")
 for idku in ${daftarid[@]};
 do
-curl -k http://fad-api.fortinet.demo:8090/news?id=$idku -H "Content-Type: application/json"
+curl -k http://fad-api.fortinet.demo/news?id=$idku -H "Content-Type: application/json"
 done
 
 
 for k in {1..11}
 do
 curldata="{\"user\":\"ahmad $k\"}"
-curl http://fad-api.fortinet.demo:8090/love/food -k -H "Content-Type: application/json" -d "$curldata"
-curl http://fad-api.fortinet.demo:8090/love/drink -k -H "Content-Type: application/json" -d "$curldata"
+curl http://fad-api.fortinet.demo/love/food -k -H "Content-Type: application/json" -d "$curldata"
+curl http://fad-api.fortinet.demo/love/drink -k -H "Content-Type: application/json" -d "$curldata"
 done
 
 
 
-curl http://fad-api.fortinet.demo:8090/101/ -k -H "Content-Type: application/json"
-curl http://fad-api.fortinet.demo:8090/102/ -k -H "Content-Type: application/json"
-curl http://fad-api.fortinet.demo:8090/1101/ -k -H "Content-Type: application/json"
-curl http://fad-api.fortinet.demo:8090/1102/ -k -H "Content-Type: application/json"
-curl http://fad-api.fortinet.demo:8090/1201/ -k -H "Content-Type: application/json"
-curl http://fad-api.fortinet.demo:8090/1202/ -k -H "Content-Type: application/json"
-curl http://fad-api.fortinet.demo:8090/1011/ -k -H "Content-Type: application/json"
-curl http://fad-api.fortinet.demo:8090/1012/ -k -H "Content-Type: application/json"
-curl http://fad-api.fortinet.demo:8090/11011/ -k -H "Content-Type: application/json"
-curl http://fad-api.fortinet.demo:8090/11012/ -k -H "Content-Type: application/json"
-curl http://fad-api.fortinet.demo:8090/12011/ -k -H "Content-Type: application/json"
-curl http://fad-api.fortinet.demo:8090/12012/ -k -H "Content-Type: application/json"
+curl http://fad-api.fortinet.demo/101/ -k -H "Content-Type: application/json"
+curl http://fad-api.fortinet.demo/102/ -k -H "Content-Type: application/json"
+curl http://fad-api.fortinet.demo/1101/ -k -H "Content-Type: application/json"
+curl http://fad-api.fortinet.demo/1102/ -k -H "Content-Type: application/json"
+curl http://fad-api.fortinet.demo/1201/ -k -H "Content-Type: application/json"
+curl http://fad-api.fortinet.demo/1202/ -k -H "Content-Type: application/json"
+curl http://fad-api.fortinet.demo/1011/ -k -H "Content-Type: application/json"
+curl http://fad-api.fortinet.demo/1012/ -k -H "Content-Type: application/json"
+curl http://fad-api.fortinet.demo/11011/ -k -H "Content-Type: application/json"
+curl http://fad-api.fortinet.demo/11012/ -k -H "Content-Type: application/json"
+curl http://fad-api.fortinet.demo/12011/ -k -H "Content-Type: application/json"
+curl http://fad-api.fortinet.demo/12012/ -k -H "Content-Type: application/json"
 
 
 
