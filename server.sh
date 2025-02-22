0545f6f9b46b93b4bc542013a691770ed4f2c665
ibsina <35305255+ibsina@users.noreply.github.com>
2025-02-22 15:49:10 +0800

    Update server.sh

--- server.sh
+++ server.sh
@@ -17,8 +17,8 @@ docker-compose --version
 docker-compose up -d
 
 # Bring up Finance Web for ML use-case
-docker run -d -p 8004:80 -v /root/partner-lab/fweb-ml-test:/usr/local/apache2/htdocs/  httpd
-docker run -d -p 8010:80 -v /root/partner-lab/test1:/usr/local/apache2/htdocs/ httpd 
-docker run -d -p 8011:80 -v /root/partner-lab/test2:/usr/local/apache2/htdocs/ httpd 
-docker run -d -p 8012:80 -v /root/partner-lab/test3:/usr/local/apache2/htdocs/ httpd 
+docker run -d -p 8004:80 -v /root/partner-lab/fweb-ml-test:/usr/local/apache2/htdocs/  httpd:alpine
+docker run -d -p 8010:80 -v /root/partner-lab/test1:/usr/local/apache2/htdocs/ httpd:alpine 
+docker run -d -p 8011:80 -v /root/partner-lab/test2:/usr/local/apache2/htdocs/ httpd:alpine 
+docker run -d -p 8012:80 -v /root/partner-lab/test3:/usr/local/apache2/htdocs/ httpd:alpine 
 docker run -d -p 8006:8088 teguhasiong/api4fun
